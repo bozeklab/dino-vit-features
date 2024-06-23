37fc03a593ee7aae0fd3b1636cee47e56cdca2f1
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-23 20:15:07 +0200

    refactor

--- part_cosegmentation.py
+++ part_cosegmentation.py
@@ -35,6 +35,7 @@ def load_from_checkpoint(ckp_path, **kwargs):
             try:
                 print(checkpoint[key].keys())
                 state_dict = {k.replace("module.", ""): v for k, v in checkpoint[key].items()}
+                state_dict = {k.replace("backbone.", ""): v for k, v in checkpoint[key].items()}
                 msg = value.load_state_dict(state_dict, strict=False)
                 print("=> loaded '{}' from checkpoint '{}' with msg {}".format(key, ckp_path, msg))
             except TypeError:
