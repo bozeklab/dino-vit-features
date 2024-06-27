c0518d2a9dd8807dcb4c27127077e7e17c3baa58
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2024-06-27 19:09:46 +0200

    refactor

--- part_cosegmentation.py
+++ part_cosegmentation.py
@@ -407,8 +407,8 @@ def draw_part_cosegmentation(num_parts: int, segmentation_parts: List[np.ndarray
         ax.axis('off')
         color_list = ["red", "yellow", "blue", "lime", "darkviolet", "magenta", "cyan", "brown", "yellow"]
         cmap = 'jet' if num_parts > 10 else ListedColormap(color_list[:(num_parts + 1)])
-        ax.imshow(checkerboard_bg, cmap='gray', vmin=0, vmax=255)
-        #ax.imshow(masked_image_transparent.astype(np.int32), vmin=0, vmax=255)
+        #ax.imshow(checkerboard_bg, cmap='gray', vmin=0, vmax=255)
+        ax.imshow(masked_image_transparent.astype(np.int32), vmin=0, vmax=255)
         ax.imshow(parts_seg, cmap=cmap, vmin=0, vmax=num_parts, alpha=0.5, interpolation='nearest')
         figures.append(fig)
     return figures
